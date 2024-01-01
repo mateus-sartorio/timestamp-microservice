116857de5307a1a37c8745fc66146e5d9a4883fa
mateus-sartorio <69646100+mateus-sartorio@users.noreply.github.com>
2024-01-01 19:38:10 +0000

    First functioning version complete

--- index.js
+++ index.js
@@ -2,31 +2,71 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
+const bodyParser = require("body-parser");
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: "hello API" });
 });
 
+app.get("/api/:year/:month/:day", (request, response) => {
+  const gmt = new Date(
+    Number(request.params.year),
+    Number(request.params.month),
+    Number(request.params.day),
+  );
 
+  console.log(gmt);
+
+  if (gmt instanceof Date && isFinite(gmt)) {
+    response.json({
+      unix: Number(request.params.unix),
+      utc: gmt.toUTCString(),
+    });
+  } else {
+    response.json({ error: "Invalid Date" });
+  }
+});
+
+app.get("/api/:unix", (request, response) => {
+  let gmt = null;
+
+  if (request.params.unix.includes("-")) {
+    gmt = new Date(request.params.unix);
+  } else {
+    gmt = new Date(Number(request.params.unix) * 1000);
+  }
+
+  if (gmt instanceof Date && isFinite(gmt)) {
+    response.json({
+      unix: Number(request.params.unix),
+      utc: gmt.toUTCString(),
+    });
+  } else {
+    response.json({ error: "Invalid Date" });
+  }
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
